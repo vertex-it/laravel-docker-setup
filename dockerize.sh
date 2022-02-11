90dc5f7e56d2d07c483fc774e4928c1e773db3bb
milivojsa <milivojsa96@gmail.com>
2022-02-11 13:05:30 +0100

    Fix gitignore overwrite in dockerize script

--- dockerize.sh
+++ dockerize.sh
@@ -6,7 +6,7 @@ echo "Starting to dockerize..."
 
 git clone --depth=1 -q $REPO_URL dockerize
 
-rm ./dockerize/.git ./dockerize/README.md ./dockerize/dockerize.sh
+rm -rf ./dockerize/.git ./dockerize/README.md ./dockerize/dockerize.sh ./dockerize/.gitignore
 
 shopt -s dotglob nullglob
 mv ./dockerize/* .
